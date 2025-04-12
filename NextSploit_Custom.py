893a161892bcf1df9420751863707d80b961ac57
Ghost <hexdex215@gmail.com>
2025-04-12 09:21:39 +0200

    Update NextSploit_Custom.py

--- NextSploit_Custom.py
+++ NextSploit_Custom.py
@@ -2,7 +2,7 @@
 # NextPloit Test File
 # Purpose: This file is intended for testing and
 # reviewing community Pull Requests without affecting
-# the main NextPloit.py script.
+# the main NextSploit.py script.
 #
 # Note: This is not the final version and may be 
 # replaced or merged later.
