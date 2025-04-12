82bda00192aef460eb1c8738624ca4cc2d386229
Ghost <hexdex215@gmail.com>
2025-04-12 09:18:42 +0200

    Create NextPloit_Custom.py

---
+++ NextPloit_Custom.py
@@ -0,0 +1,9 @@
+# ============================================
+# NextPloit Test File
+# Purpose: This file is intended for testing and
+# reviewing community Pull Requests without affecting
+# the main NextPloit.py script.
+#
+# Note: This is not the final version and may be 
+# replaced or merged later.
+# ============================================
